9b134c4bfc5960f84cddf8c2c0657ece723f747c
hatvalues <julian@hatvalues.org>
2023-03-08 18:59:53 +0100

    updated tests for fmla_funcs

--- tests/testthat/test-fmla_funcs.R
+++ tests/testthat/test-fmla_funcs.R
@@ -1,3 +1,25 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+nims <- c("x1", "x2", "x3")
+dframe <- data.frame(c(1, 1, -1), c("a", "b", "c"), runif(3), rnorm(3))
+names(dframe) <- c("y", nims)
+
+fmla <- fmla_from_names("y", nims)
+test_that("fmla is valid fmla", {
+  expect_s3_class(fmla, "formula")
 })
+test_that("fmla is valid fmla", {
+  expect_s3_class(lm(fmla, dframe), "lm")
+})
+
+
+fmla <- fmla_from_dframe("y", dframe)
+test_that("fmla is valid fmla", {
+  expect_s3_class(fmla, "formula")
+})
+test_that("fmla is valid fmla", {
+  expect_s3_class(lm(fmla, dframe), "lm")
+})
+
+
+
+
+
